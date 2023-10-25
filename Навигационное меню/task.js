5a460c20fa716f558717559bf6dc0a06b63f3351
MariaD04 <117981238+MariaD04@users.noreply.github.com>
2023-10-26 01:52:11 +0400

    Update task.js

--- Навигационное меню/task.js
+++ Навигационное меню/task.js
@@ -1,21 +1,22 @@
-const menuMain = document.querySelector('.menu_main');
+const submenuAll = Array.from(document.querySelectorAll('.menu_sub'));
 
-menuMain.addEventListener('click', event => {
-  const menuLink = event.target;
-  const menuItem = menuLink.closest('.menu__item')
-  
-  if (menuItem.querySelector('.menu_sub')) {
-    const submenu = menuItem.querySelector('.menu_sub');
+submenuAll.forEach ( submenu => {
+  const menuItem = submenu.closest('.menu__item');
+  const menuLink = menuItem.querySelector('.menu__link');
+  menuLink.addEventListener('click', (event) => {
     event.preventDefault();
-    submenu.classList.toggle('menu_active');
-    const submenuLinks = submenu.querySelectorAll('.menu__link');
-    submenuLinks.forEach(link => {
-      link.onclick = () => {
-        submenu.classList.remove('menu_active');
-        return false;
-      }
-    }); 
-  }
+    const currentSubmenu = event.target.nextElementSibling;
+    const openSubmenu = document.querySelector('.menu_sub.menu_active');
+    if (openSubmenu && openSubmenu.isEqualNode(currentSubmenu)) {
+      openSubmenu.classList.remove('menu_active');
+    } else if (openSubmenu) {
+      openSubmenu.classList.remove('menu_active');
+      currentSubmenu.classList.add('menu_active');
+    } else {
+      currentSubmenu.classList.add('menu_active');
+    }
+    return false;
+  }) 
 })
 
 
